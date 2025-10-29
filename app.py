d6a6995748e90f21cc613179182b71a901d7a4ea
Ktoxz <69800528+ktoxz@users.noreply.github.com>
2025-10-30 03:37:56 +0700

    change port

--- app.py
+++ app.py
@@ -678,5 +678,5 @@ def inject_template_vars():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5021))
     app.run(host="0.0.0.0", port=port)
